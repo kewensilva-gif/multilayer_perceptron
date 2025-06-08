a7d4319023cb2ee7a01de5d17ecf304ed5f4ea8b
KEWEN SILVA <kewensilva58@gmail.com>
2025-06-07 22:02:09 -0300

    implementa o kfolds com a matriz de confusão

--- training_mlp.py
+++ training_mlp.py
@@ -7,9 +7,11 @@ from torch.utils.data import DataLoader
 from mlp import MLP
 
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from torch.utils.data import Subset
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -20,18 +22,12 @@ transform = transforms.Compose([
     # transforms.Normalize((0.5,), (0.5,))
 ])
 
-# Dataset e DataLoader
+# Dataset
 train_dataset = datasets.ImageFolder(root="dataset-train", transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
 num_classes = len(train_dataset.classes)
 class_names = train_dataset.classes
 
-# Modelo, perda e otimizador
-model = MLP(num_classes).to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-def training(min_error=0.3):
+def training(model, criterion, optimizer, train_loader_fold, min_error=0.03):
     epoch = 0
     max_epochs = 300
     while True:
@@ -40,7 +36,7 @@ def training(min_error=0.3):
         correct = 0
         total = 0
 
-        for images, labels in train_loader:
+        for images, labels in train_loader_fold:
             images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = model(images)
@@ -53,12 +49,12 @@ def training(min_error=0.3):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-        epoch_error = cumulative_batch_error / len(train_loader)
+        epoch_error = cumulative_batch_error / len(train_loader_fold)
         epoch_accuracy = 100 * correct / total
 
         print(f"Época {epoch+1} | Loss: {epoch_error:.4f} | Acurácia: {epoch_accuracy:.2f}%")
 
-        if epoch_error <= min_error or epoch >= max_epochs -1:
+        if (epoch_error <= min_error and epoch_accuracy == 100) or epoch >= max_epochs -1:
             if epoch_error <= min_error:
                 print(f"Critério de erro baixo ({epoch_error:.4f} <= {min_error}) atingido.")
             if epoch >= max_epochs -1:
@@ -76,7 +72,6 @@ def plot_confusion_matrix(cm, class_names_list):
     plt.show()
 
 def evaluate_model(model, loader, device, class_names_list):
-    print("\nAvaliação do modelo")
     model.eval()
     all_labels = []
     all_preds = []
@@ -90,11 +85,6 @@ def evaluate_model(model, loader, device, class_names_list):
             all_labels.extend(labels.cpu().numpy())
             all_preds.extend(predicted.cpu().numpy())
 
-    # all_labels_clean = [int(label) for label in all_labels]
-    # all_preds_clean = [int(pred) for pred in all_preds]
-    # print("labels",all_labels_clean)
-    # print("preds",all_preds_clean)
-
     cm = confusion_matrix(all_labels, all_preds)
     print("\nMatriz de Confusão:")
     print(cm)
@@ -102,15 +92,28 @@ def evaluate_model(model, loader, device, class_names_list):
 
     return cm
 
+def training_kfolds(batch_size, k, learning_rate=0.1):
+    kf = KFold(n_splits=k, shuffle=True, random_state=0)
+
+    for fold, (train_id, val_id) in enumerate(kf.split(train_dataset)):
+        print(f"\n===== FOLD {fold + 1}/{k} =====")
+
+        train_subset = Subset(train_dataset, train_id)
+        val_subset = Subset(train_dataset, val_id)
+
+        train_loader_fold = DataLoader(train_subset, batch_size=batch_size, shuffle=True)
+        val_loader_fold = DataLoader(val_subset, batch_size=batch_size, shuffle=False)
+
+        # A cada fold é criado um novo modelo
+        model = MLP(num_classes).to(device)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), learning_rate)
+
+        training(model, criterion, optimizer, train_loader_fold)
+
+        print(f"\nAvaliação do modelo no Fold {fold + 1}")
+        evaluate_model(model, val_loader_fold, device, class_names)
+
 if __name__ == '__main__':
-    training()
-    print("Treinamento da MLP concluído!")
-
-    # Salvar modelo
-    torch.save(model.state_dict(), "modelo_mlp.pth")
-    print("Modelo MLP salvo com sucesso!")
-
-    # Testar modelo
-    test_dataset = datasets.ImageFolder(root="dataset-test", transform=transform)
-    test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
-    evaluate_model(model, test_loader, device, class_names)
+    training_kfolds(10, 5)
+    print("\nValidação finalizada")
